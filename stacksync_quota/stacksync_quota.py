778732eef47be9d89928ed83dae2d7171483d876
Edgar Zamora Gómez <cesk002@gmail.com>
2015-03-06 17:46:17 +0100

    Prepare the code to connect to server using OMQ. Filter PUTs and DELETs. Implement the logic when a put arrive.

--- stacksync_quota/stacksync_quota.py
+++ stacksync_quota/stacksync_quota.py
@@ -1,26 +1,30 @@
-'''
-Created on 26 Feb 2015
 
-@author: cotes
-'''
 import os
 from swift.common.swob import wsgify, HTTPUnauthorized, HTTPBadRequest
+from util import create_response, is_valid_status, create_error_response
+
 import xmlrpclib
 from swift.common.utils import get_logger
 import json
+from client import Client
 
 class StackSyncQuotaMiddleware(object):
     def __init__(self, app, conf):
         self.app = app
-        host = conf.get('stacksync_host', '127.0.0.1').lower()
-        port = conf.get('stacksync_port', 61234)
-        self.rpc_server = xmlrpclib.ServerProxy("http://"+host+":"+str(port))
-        self.app.logger = get_logger(conf, log_route='stacksync_api')
+        host = conf.get('omq_host', '127.0.0.1').lower()
+        port = int(conf.get('omq_port', 62345))
+#         user = conf.get('omq_user', 'guest')
+#         password = conf.get('omq_pass', 'guest')
+#         exchange = conf.get('omq_exchange', 'rpc_global_exchange')
+#         self.binding_name = conf.get('binding_name', 'IOmqQuotaHandler')
+        self.rpc_server.XmlRpcSyncHandler.getAvailableQuota(req.environ["HTTP_X_USER"])     
         self.app.logger.info('StackSync Quota: Init OK')
+#         env = {'user': user, 'pass': password, 'host' : host, 'port': port, 'exchange': exchange}
+#         self.client = Client(env)
 
     @wsgify
     def __call__(self, req):
-        self.app.logger.info('StackSync Qutoa start')
+        self.app.logger.info('StackSync Quota start')
         self.app.logger.info(req.environ)
 
         #response = self.authorize(req)
@@ -32,11 +36,48 @@ class StackSyncQuotaMiddleware(object):
         #self.app.logger.info('user_id: '+str(req.environ['HTTP_X_USER_ID']))
         #self.app.logger.info('user: '+str(req.environ['HTTP_X_USER']))
         #self.app.logger.info('user_name: '+str(req.environ['HTTP_X_USER_NAME']))
-        response = self.rpc_server.XmlRpcSyncHandler.getAvailableQuota(req.environ["HTTP_X_USER"])
-        
-        self.app.logger.info(response)
-        return self.app
+        if self.valid_request(req):
+            quota_info = self.rpc_server.XmlRpcHandler.getAvailableQuota(req.environ["HTTP_X_USER"])
+            #quota_info = self.client.sync_call(self.binding_name, "getAvailableQuota", [req.environ["HTTP_X_USER"]])
+
+            response = create_response(quota_info, status_code=200)
+            
+            if not is_valid_status(response.status_int):
+                app.logger.error("StackSync Quota: status code: %s. body: %s", str(response.status_int), str(response.body))
+                return response
+            
+            quota_info = json.loads(quota_info)
+            
+            if req.method == 'PUT':
+                return self.add_quota_used(quota_info, req.environ["CONTENT_LENGTH"])
+            if req.method == 'DELETE':
+                return self.substract_quota_used(quota_info)
+            
+                                    
     
+    def add_quota_used(self, quota_info, content_uploaded):
+        quota_limit = quota_info['quota_limit']
+        quota_used = quota_info['quota_used']
+        
+        quota_used_afet_put = quota_used + content_uploaded
+        if quota_used_afet_put > quota_limit:
+            return create_error_response(401, 'You have exceeded the maximum quota. Your available space is: '+(quota_limit-quota_used))
+        else:
+            #Notify quota_server for the new quota_used value.
+            #self.rpc_server.XmlRpcHandler.updateAvailableQuota(quota_info['user'], quota_used_after_put)
+
+            return self.app
+        
+    def substract_quota_used(self, quota_inf):
+        # HEAD to swift resource to know the content length
+        
+        return self.app
+        
+    def valid_request(self, req):
+        if (req.method == 'PUT' or req.method == 'DELETE'):
+            return True                                                          
+        
+        return False
     def authorize(self, req):
         self.app.logger.info('StackSync API: authorize: path info: %s', req.path)
         if 'swift.authorize' in req.environ:
