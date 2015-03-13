9f6a5fde4f24f2545cdd0bb0f47888cc88a27edf
Edgar Zamora Gómez <cesk002@gmail.com>
2015-03-13 16:52:43 +0100

    Fix errors

--- stacksync_quota/stacksync_quota.py
+++ stacksync_quota/stacksync_quota.py
@@ -1,6 +1,8 @@
 
 import os
 from swift.common.swob import wsgify, HTTPUnauthorized, HTTPBadRequest
+from swift.common.wsgi import make_pre_authed_request
+
 from util import create_response, is_valid_status, create_error_response
 
 import xmlrpclib
@@ -17,7 +19,8 @@ class StackSyncQuotaMiddleware(object):
 #         password = conf.get('omq_pass', 'guest')
 #         exchange = conf.get('omq_exchange', 'rpc_global_exchange')
 #         self.binding_name = conf.get('binding_name', 'IOmqQuotaHandler')
-        self.rpc_server.XmlRpcSyncHandler.getAvailableQuota(req.environ["HTTP_X_USER"])     
+        self.app.logger = get_logger(conf, log_route='stacksync_quota')
+        self.rpc_server = xmlrpclib.ServerProxy("http://"+host+":"+str(port))
         self.app.logger.info('StackSync Quota: Init OK')
 #         env = {'user': user, 'pass': password, 'host' : host, 'port': port, 'exchange': exchange}
 #         self.client = Client(env)
@@ -37,39 +40,70 @@ class StackSyncQuotaMiddleware(object):
         #self.app.logger.info('user: '+str(req.environ['HTTP_X_USER']))
         #self.app.logger.info('user_name: '+str(req.environ['HTTP_X_USER_NAME']))
         if self.valid_request(req):
-            quota_info = self.rpc_server.XmlRpcHandler.getAvailableQuota(req.environ["HTTP_X_USER"])
+            quota_info = self.rpc_server.XmlRpcQuotaHandler.getAvailableQuota(req.environ["HTTP_X_USER"])
             #quota_info = self.client.sync_call(self.binding_name, "getAvailableQuota", [req.environ["HTTP_X_USER"]])
 
             response = create_response(quota_info, status_code=200)
             
             if not is_valid_status(response.status_int):
-                app.logger.error("StackSync Quota: status code: %s. body: %s", str(response.status_int), str(response.body))
+                self.app.logger.error("StackSync Quota: status code: %s. body: %s", str(response.status_int), str(response.body))
                 return response
             
             quota_info = json.loads(quota_info)
             
             if req.method == 'PUT':
-                return self.add_quota_used(quota_info, req.environ["CONTENT_LENGTH"])
+                return self.add_quota_used(quota_info, int(req.environ["CONTENT_LENGTH"]))
             if req.method == 'DELETE':
-                return self.substract_quota_used(quota_info)
+                return self.subtract_quota_used(quota_info, req.environ)
+        
+        return self.app
             
                                     
     
     def add_quota_used(self, quota_info, content_uploaded):
-        quota_limit = quota_info['quota_limit']
-        quota_used = quota_info['quota_used']
+        quota_limit = int(quota_info['quota_limit'])
+        quota_used = int(quota_info['quota_used'])
+        quota_used_afer_put = quota_used + int(content_uploaded)
         
-        quota_used_afet_put = quota_used + content_uploaded
-        if quota_used_afet_put > quota_limit:
-            return create_error_response(401, 'You have exceeded the maximum quota. Your available space is: '+(quota_limit-quota_used))
+        quota_used_after_put = quota_used + content_uploaded
+        if quota_used_after_put > quota_limit:
+            app.logger.error("StackSync Quota: You have exceeded the maximum quota. Your available space is: "+str(quota_limit-quota_used))
+            return create_error_response(401, 'You have exceeded the maximum quota. Your available space is: '+str(quota_limit-quota_used))
         else:
             #Notify quota_server for the new quota_used value.
             #self.rpc_server.XmlRpcHandler.updateAvailableQuota(quota_info['user'], quota_used_after_put)
-
+            self.app.logger.info('StackSync Quota: add_quota_used')
+            response = self.rpc_server.XmlRpcQuotaHandler.updateAvailableQuota(quota_info['user'], str(quota_used_after_put))
+        
+            response = create_response(response, status_code=200)
+                
+            if not is_valid_status(response.status_int):
+                self.app.logger.error("StackSync Quota: Error updating quota used")
+                return response
+        
             return self.app
         
-    def substract_quota_used(self, quota_inf):
+    def subtract_quota_used(self, quota_info, env):
         # HEAD to swift resource to know the content length
+        quota_limit = int(quota_info['quota_limit'])
+        quota_used = int(quota_info['quota_used'])
+        
+        head_resp = make_pre_authed_request(env, method='HEAD', path=env['PATH_INFO'],
+                                              agent='%(orig)s')
+        head_resp = head_resp.get_response(self.app)
+        content_to_delete = int(head_resp.headers['content-length'])
+        
+        quota_used_after_delete = quota_used - content_to_delete
+        
+        #send new quota to  
+        self.app.logger.info('StackSync Quota: subtract_quota_used')
+        response = self.rpc_server.XmlRpcQuotaHandler.updateAvailableQuota(quota_info['user'], str(quota_used_after_delete))
+        
+        response = create_response(response, status_code=200)
+            
+        if not is_valid_status(response.status_int):
+            self.app.logger.error("StackSync Quota: Error updating quota used")
+            return response
         
         return self.app
         
@@ -78,6 +112,7 @@ class StackSyncQuotaMiddleware(object):
             return True                                                          
         
         return False
+    
     def authorize(self, req):
         self.app.logger.info('StackSync API: authorize: path info: %s', req.path)
         if 'swift.authorize' in req.environ:
